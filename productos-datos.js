f8c1276816fffa384348687fb500bc49d0e9d30f
Lienzzo <edd.19.ear@gmail.com>
2026-06-29 09:26:52 -0600

    Add files via upload

---
+++ productos-datos.js
@@ -0,0 +1,354 @@
+window.LIENZZO_PRODUCTS = [
+  {
+    "id": "lib-dibujo-estandar",
+    "nombre": "Libreta de dibujo estándar",
+    "categoria": "Libretas",
+    "precio": 80,
+    "stock": null,
+    "etiqueta": "Dibujo",
+    "descripcion": "Libreta cocida de dibujo con gramaje de 200 g. Ideal para bocetos, ideas creativas y prácticas de dibujo.",
+    "imagenes": [
+      "images/productos/producto-38.png",
+      "images/productos/producto-39.png"
+    ]
+  },
+  {
+    "id": "mini-libreta-dibujo",
+    "nombre": "Mini libreta de dibujo",
+    "categoria": "Libretas",
+    "precio": 40,
+    "stock": null,
+    "etiqueta": "Mini",
+    "descripcion": "Libreta de dibujo mini con gramaje de 200 g. Pequeña, práctica y lista para llevar tus ideas a todos lados.",
+    "imagenes": [
+      "images/productos/producto-33.png",
+      "images/productos/producto-34.png"
+    ]
+  },
+  {
+    "id": "libreta-365-days",
+    "nombre": "Libreta 365 days",
+    "categoria": "Libretas",
+    "precio": 180,
+    "stock": null,
+    "etiqueta": "Especial",
+    "descripcion": "Libreta cocida con hojas papel craft y grabado de los 365 días del año en cada hoja. Incluye un bonito candado de seguridad.",
+    "imagenes": [
+      "images/productos/producto-04.png",
+      "images/productos/producto-33.png",
+      "images/productos/producto-34.png"
+    ]
+  },
+  {
+    "id": "libreta-hojas-negras",
+    "nombre": "Libreta de hojas negras",
+    "categoria": "Libretas",
+    "precio": 120,
+    "stock": null,
+    "etiqueta": "Holográfica",
+    "descripcion": "Libreta de hojas negras para escribir, dibujar y crear con bolígrafos metálicos, gel blanco, colores pastel y marcadores especiales. Portada brillante con delicados diseños holográficos.",
+    "imagenes": [
+      "images/productos/producto-28.png",
+      "images/productos/producto-29.png",
+      "images/productos/producto-30.png",
+      "images/productos/producto-31.png",
+      "images/productos/producto-32.png"
+    ]
+  },
+  {
+    "id": "libretas-colorear",
+    "nombre": "Libretas para colorear",
+    "categoria": "Libretas",
+    "precio": 80,
+    "stock": null,
+    "etiqueta": "Kawaii",
+    "descripcion": "Llena de adorables capibaras para pintar, crear y divertirte. Perfecta para pequeños artistas y amantes de lo kawaii.",
+    "imagenes": [
+      "images/productos/producto-02.png"
+    ]
+  },
+  {
+    "id": "libretas-acuarela",
+    "nombre": "Libretas para acuarela",
+    "categoria": "Libretas",
+    "precio": 100,
+    "stock": null,
+    "etiqueta": "Acuarela",
+    "descripcion": "Bloc de papel para acuarela de alta calidad, ideal para pintar, practicar técnicas y dar vida a tus ideas con color.",
+    "imagenes": [
+      "images/productos/producto-36.png",
+      "images/productos/producto-37.png"
+    ]
+  },
+  {
+    "id": "planners",
+    "nombre": "Planners",
+    "categoria": "Libretas",
+    "precio": 120,
+    "stock": null,
+    "etiqueta": "Organización",
+    "descripcion": "Planner elegante con hojas decoradas y diseños inspirados en la naturaleza y el arte. Ideal para organizar tus días con un toque creativo y sofisticado.",
+    "imagenes": [
+      "images/productos/producto-00.png",
+      "images/productos/producto-03.png"
+    ]
+  },
+  {
+    "id": "libretas-decoradas",
+    "nombre": "Libretas decoradas",
+    "categoria": "Libretas",
+    "precio": 90,
+    "stock": null,
+    "etiqueta": "Decorada",
+    "descripcion": "Añade dulzura y creatividad a tus notas con esta encantadora libreta de hojas decoradas.",
+    "imagenes": [
+      "images/productos/producto-08.png",
+      "images/productos/producto-09.png"
+    ]
+  },
+  {
+    "id": "libreta-van-gogh",
+    "nombre": "Libreta de Van Gogh",
+    "categoria": "Libretas",
+    "precio": 110,
+    "stock": null,
+    "etiqueta": "Arte",
+    "descripcion": "Libreta de 200 hojas con portada inspirada en el inconfundible estilo de Van Gogh. Ideal para apuntes, ideas, planificación o creatividad diaria.",
+    "imagenes": [
+      "images/productos/producto-21.png",
+      "images/productos/producto-22.png",
+      "images/productos/producto-23.png",
+      "images/productos/producto-24.png",
+      "images/productos/producto-25.png",
+      "images/productos/producto-26.png",
+      "images/productos/producto-27.png"
+    ]
+  },
+  {
+    "id": "mini-libreta-decorada",
+    "nombre": "Mini libreta decorada",
+    "categoria": "Libretas",
+    "precio": 65,
+    "stock": null,
+    "etiqueta": "Pasta dura",
+    "descripcion": "Libretas de pasta dura con diseños elegantes y hojas decoradas. Perfectas para journaling, organizar ideas o guardar recuerdos.",
+    "imagenes": [
+      "images/productos/producto-03.png",
+      "images/productos/producto-08.png",
+      "images/productos/producto-09.png"
+    ]
+  },
+  {
+    "id": "block-mediano-dibujo",
+    "nombre": "Block mediano de dibujo",
+    "categoria": "Libretas",
+    "precio": 150,
+    "stock": null,
+    "etiqueta": "120 g / 160 g",
+    "descripcion": "Block de espiral con portada bonita. Disponible en 120 g y 160 g, con hojas listas para ideas, bocetos y colores.",
+    "imagenes": [
+      "images/productos/producto-35.png",
+      "images/productos/producto-36.png",
+      "images/productos/producto-37.png"
+    ]
+  },
+  {
+    "id": "block-dibujo-mediano-200",
+    "nombre": "Block de dibujo mediano",
+    "categoria": "Libretas",
+    "precio": 200,
+    "stock": null,
+    "etiqueta": "200 g",
+    "descripcion": "Block de dibujo de pasta dura, hojas de 200 g con línea de recorte y un diseño que hace que cada dibujo se sienta especial.",
+    "imagenes": [
+      "images/productos/producto-38.png",
+      "images/productos/producto-39.png"
+    ]
+  },
+  {
+    "id": "block-dibujo-grande-160",
+    "nombre": "Block de dibujo grande 160 g",
+    "categoria": "Libretas",
+    "precio": 160,
+    "stock": null,
+    "etiqueta": "160 g",
+    "descripcion": "Block de dibujo de 160 g con hojas blancas y línea de recorte, hecho para ilustración, acuarela, marcador o boceto.",
+    "imagenes": [
+      "images/productos/producto-35.png",
+      "images/productos/producto-36.png"
+    ]
+  },
+  {
+    "id": "block-dibujo-grande-240",
+    "nombre": "Block de dibujo grande 240 g",
+    "categoria": "Libretas",
+    "precio": 160,
+    "stock": null,
+    "etiqueta": "240 g",
+    "descripcion": "Block de dibujo de 240 g con hojas blancas y línea de recorte para crear ilustraciones, prácticas y proyectos artísticos.",
+    "imagenes": [
+      "images/productos/producto-37.png",
+      "images/productos/producto-38.png"
+    ]
+  },
+  {
+    "id": "libreta-dibujo-espacial",
+    "nombre": "Libreta de dibujo espacial",
+    "categoria": "Libretas",
+    "precio": 130,
+    "stock": null,
+    "etiqueta": "Espacial",
+    "descripcion": "Un espacio para dibujar, crear y perderte entre las estrellas. Libreta de dibujo de pasta dura con diseño espacial.",
+    "imagenes": [
+      "images/productos/producto-40.png",
+      "images/productos/producto-41.png",
+      "images/productos/producto-42.png"
+    ]
+  },
+  {
+    "id": "plumas-punta-fina",
+    "nombre": "Plumas marcador de punta fina",
+    "categoria": "Plumas",
+    "precio": 100,
+    "stock": null,
+    "etiqueta": "12 tonos",
+    "descripcion": "Plumas marcadores de punta pincel suave, ideales para lettering, ilustración, apuntes creativos y decoración. Incluye 12 tonos vibrantes.",
+    "imagenes": [
+      "images/productos/producto-12.png",
+      "images/productos/producto-17.png",
+      "images/productos/producto-18.png"
+    ]
+  },
+  {
+    "id": "mega-kit-lapices-colores",
+    "nombre": "Mega kit de lápices de colores",
+    "categoria": "Colores",
+    "precio": 320,
+    "stock": null,
+    "etiqueta": "Mega kit",
+    "descripcion": "Colores intensos, trazos suaves y libertad para corregir. Perfectos para dibujar, colorear y crear sin límites.",
+    "imagenes": [
+      "images/productos/producto-07.png"
+    ]
+  },
+  {
+    "id": "lapices-colores",
+    "nombre": "Lápices de colores",
+    "categoria": "Colores",
+    "precio": 60,
+    "stock": null,
+    "etiqueta": "Escolar",
+    "descripcion": "Pequeños en tamaño y grandes en creatividad. Set perfecto para la escuela o dibujo, con colores vibrantes y dos tonos metálicos.",
+    "imagenes": [
+      "images/productos/producto-05.png",
+      "images/productos/producto-06.png"
+    ]
+  },
+  {
+    "id": "plumones-acrilicos-pastel",
+    "nombre": "Plumones acrílicos en tonos pastel",
+    "categoria": "Plumones",
+    "precio": 120,
+    "stock": null,
+    "etiqueta": "24 colores",
+    "descripcion": "Paleta suave, delicada y llena de encanto. Plumones acrílicos pastel de 24 colores para decorar libretas, personalizar proyectos, hacer lettering o añadir un toque aesthetic.",
+    "imagenes": [
+      "images/productos/producto-10.png",
+      "images/productos/producto-11.png",
+      "images/productos/producto-12.png"
+    ]
+  },
+  {
+    "id": "plumones-acrilicos-surtidos",
+    "nombre": "Plumones acrílicos surtidos",
+    "categoria": "Plumones",
+    "precio": 340,
+    "stock": null,
+    "etiqueta": "72 piezas",
+    "descripcion": "Colección para crear sin límites: 72 plumones acrílicos distribuidos en 24 tonos pastel, 24 colores intensos y 24 metálicos.",
+    "imagenes": [
+      "images/productos/producto-13.png",
+      "images/productos/producto-14.png",
+      "images/productos/producto-19.png"
+    ]
+  },
+  {
+    "id": "plumones-acrilicos-semi",
+    "nombre": "Plumones acrílicos semi-profesionales",
+    "categoria": "Plumones",
+    "precio": 515,
+    "stock": null,
+    "etiqueta": "60 tonos",
+    "descripcion": "Set de 60 tonos armónicos para ilustración, lettering, manualidades y proyectos artísticos. Una selección equilibrada de colores vibrantes y combinables.",
+    "imagenes": [
+      "images/productos/producto-15.png",
+      "images/productos/producto-18.png",
+      "images/productos/producto-20.png"
+    ]
+  },
+  {
+    "id": "plumones-acrilicos-mini",
+    "nombre": "Plumones acrílicos mini",
+    "categoria": "Plumones",
+    "precio": 65,
+    "stock": null,
+    "etiqueta": "12 tonos",
+    "descripcion": "Set mini de 12 tonos pastel: pequeño, práctico y perfecto para decorar, dibujar, hacer lettering y darle un toque aesthetic a cada proyecto.",
+    "imagenes": [
+      "images/productos/producto-16.png"
+    ]
+  },
+  {
+    "id": "plumones-acrilicos-lavables",
+    "nombre": "Plumones acrílicos lavables",
+    "categoria": "Plumones",
+    "precio": 250,
+    "stock": null,
+    "etiqueta": "48 colores",
+    "descripcion": "Set de 48 marcadores acrílicos lavables con tonos vibrantes para dibujar, decorar y crear sin preocupaciones.",
+    "imagenes": [
+      "images/productos/producto-18.png",
+      "images/productos/producto-19.png",
+      "images/productos/producto-20.png"
+    ]
+  },
+  {
+    "id": "kit-scrapbook",
+    "nombre": "Kit de scrapbook",
+    "categoria": "Scrapbook",
+    "precio": 200,
+    "stock": null,
+    "etiqueta": "Kit completo",
+    "descripcion": "Papeles, tarjetas, stickers y elementos decorativos con temática única para decorar, crear y personalizar tus proyectos.",
+    "imagenes": [
+      "images/productos/producto-01.png"
+    ]
+  },
+  {
+    "id": "block-stickers",
+    "nombre": "Block de stickers",
+    "categoria": "Scrapbook",
+    "precio": 100,
+    "stock": null,
+    "etiqueta": "Stickers",
+    "descripcion": "Blocks de stickers temáticos con diseños inspirados en estilos vintage, naturaleza, lectura, café y más. Perfectos para journals, agendas, cartas y álbumes.",
+    "imagenes": [
+      "images/productos/producto-08.png",
+      "images/productos/producto-09.png"
+    ]
+  },
+  {
+    "id": "libro-paisajes",
+    "nombre": "Libro de paisajes",
+    "categoria": "Scrapbook",
+    "precio": 65,
+    "stock": null,
+    "etiqueta": "Decorativo",
+    "descripcion": "Libro de imágenes decorativas inspirado en arte, naturaleza y paisajes. Ideal para journaling, scrapbooking, collages y manualidades.",
+    "imagenes": [
+      "images/productos/producto-01.png",
+      "images/productos/producto-08.png",
+      "images/productos/producto-09.png"
+    ]
+  }
+];
